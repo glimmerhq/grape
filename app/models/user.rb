88febd1c45f4680f533146d8655d9a67a9cf58b8
Luna <her@mint.lgbt>
2021-04-14 13:50:27 +0000

    Add Relationships to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,13 @@ class User < ApplicationRecord
   # only allow letter, number, underscore and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :following_users, through: :relationships, source: :following
+  has_many :reverse_relationships, foreign_key: "following_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   def self.find_for_database_authentication warden_condition
     conditions = warden_condition.dup
     login = conditions.delete(:login)
@@ -22,5 +29,13 @@ class User < ApplicationRecord
 
   def login
     @login || self.username || self.email
+  end
+
+  def following?(other_user)
+    relationships.find_by_following_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(following_id: other_user.id)
   end
 end
